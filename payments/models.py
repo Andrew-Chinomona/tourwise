424783fb877f06713d388f3bc07189db720ee082
Andrew <andrewtchinomona@gmail.com>
2025-06-16 16:57:45 +0200

    I have finished the base app for payments, I have created the core model -payments, i have registered the model in payments/admin.py and i ahve run migrations and created template and static folders to prepare the app for processes

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.conf import settings
+from listings.models import Property
 # Create your models here.
+class Payment(models.Model):
+    LISTING_TYPE_CHOICES = (
+    ('normal', 'Normal'),
+    ('priority', 'Priority'),
+    )
+
+    property = models.OneToOneField(Property, on_delete=models.CASCADE)
+    user =models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    listing_type = models.CharField(max_length=10, choices=LISTING_TYPE_CHOICES)
+    amount = models.DecimalField(decimal_places=2, max_digits=6)
+    is_complete = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.property.title} - {self.listing_type} - ${self.amount}"
